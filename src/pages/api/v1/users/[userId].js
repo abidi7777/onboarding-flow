fe4facee3a599be6f978b4fcd2ef85a005e5696e
abidi <107292069+abidi7777@users.noreply.github.com>
2022-09-09 22:19:48 +0530

    bug fix

--- src/pages/api/v1/users/[userId].js
+++ src/pages/api/v1/users/[userId].js
@@ -41,7 +41,6 @@ export default async function handler(req, res) {
       res.status(200).json({ data: db.data.users.find((user) => user.userId === userId) });
     }
   } catch (e) {
-    console.log(e);
     res.status(200).json({ data: req.body });
   }
 }
