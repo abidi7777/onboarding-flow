be87a9b8d44b5a12d0de987edcac3739e0e61834
abidi <107292069+abidi7777@users.noreply.github.com>
2022-09-09 21:39:33 +0530

    updated products

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -10,6 +10,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="./vercel-icon-dark.svg" type="image/svg" sizes="16x16 32x32 96x96 180x180 300x300 512x512" />
       </Head>
       <body>
         <Main />
