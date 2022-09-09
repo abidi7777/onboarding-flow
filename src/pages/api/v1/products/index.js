97cf0516c2803a7f8558d25d41d24f0e9a8a0e52
abidi <107292069+abidi7777@users.noreply.github.com>
2022-09-09 21:04:29 +0530

    lint

--- src/pages/api/v1/products/index.js
+++ src/pages/api/v1/products/index.js
@@ -1,5 +1,5 @@
 import products from '../../data/products.json';
 
-export default async function handler(req, res) {
+export default async function handler(_, res) {
   res.status(200).json(products);
 }
