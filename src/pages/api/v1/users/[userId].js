76f71e7d385007496444dee8738eabc3ab86da58
abidi <107292069+abidi7777@users.noreply.github.com>
2022-09-09 22:12:37 +0530

    update response

--- src/pages/api/v1/users/[userId].js
+++ src/pages/api/v1/users/[userId].js
@@ -41,6 +41,6 @@ export default async function handler(req, res) {
       res.status(200).json({ data: db.data.users.find((user) => user.userId === userId) });
     }
   } catch (e) {
-    res.status(500).json({ error: e.message });
+    res.status(200).json({ data: req.body });
   }
 }
